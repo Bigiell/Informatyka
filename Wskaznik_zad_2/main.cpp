0d49ec749d04829f9bcdbfde4efba6fab0708c23
Bigiell <32448080+Bigiell@users.noreply.github.com>
2017-10-06 11:34:10 +0200

    Zadanie z przesunięciem bitowym

---
+++ Wskaznik_zad_2/main.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int zw=2;
+
+    printf("2<<1 wynik przesuniecia w lewo %d\n ", zw<<1); // wypisanie wyniku
+    printf("2<<2 wynik przesuniecia w lewo %d\n ", zw<<2); // wypisanie wyniku
+    printf("2<<3 wynik przesuniecia w lewo %d\n", zw<<3); // wypisanie wyniku
+    return 0;
+}
